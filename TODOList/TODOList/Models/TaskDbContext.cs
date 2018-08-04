dd0667958b1d51d437bc5e103bd58bdb8192bb9a
Emo <eiv.razgrad@gmail.com>
2018-08-04 15:40:53 +0300

    TODO list application

---
+++ TODOList/TODOList/Models/TaskDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace TODOList.Models
+{
+    public class TaskDbContext : DbContext
+    {
+        public virtual DbSet<Task> Tasks { get; set; }
+        public TaskDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=todo;user=root;");
+        }
+    }
+}
